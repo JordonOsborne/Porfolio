ea3795b97caf0068edaca7ec0e4154b10975f05b
Jordon Osborne <jlosborne52@gmail.com>
2023-06-09 20:51:40 -0400

    Basic project card and page completed.

--- pages/Projects.jsx
+++ pages/Projects.jsx
@@ -1,3 +1,41 @@
+import Header from '../Components/Header'
+import UserPanel from '../Components/UserPanel'
+import ProjectCard from '../Components/ProjectCard'
+import Loading from '../Components/Reusable/Loading'
+import styles from '../styles/Admin.module.scss'
+import FirebaseAPI from '../Context/FirebaseAPI'
+import { useEffect, useContext } from 'react'
+
 export default function Projects() {
-  return <div>Projects</div>
+	const { GetData, data } = useContext(FirebaseAPI)
+
+	useEffect(() => {
+		GetData('Projects')
+	}, [])
+
+	console.log(data)
+
+	return (
+		<div id='Page'>
+			<Header />
+			<div id={styles.Admin}>
+				<UserPanel />
+				<main>
+					<div id='Company'>
+						<div
+							id={styles.Projects}
+							className={styles.Grid}
+						>
+							{data.map((Project) => (
+								<ProjectCard
+									Info={Project}
+									key={Project.id}
+								/>
+							))}
+						</div>
+					</div>
+				</main>
+			</div>
+		</div>
+	)
 }
--- pages/Projects/[Id]/index.jsx
+++ pages/Projects/[Id]/index.jsx
@@ -1,11 +1,12 @@
-import styles from '../../../styles/Admin.module.scss'
+import styles from '../../../styles/Project.module.scss'
 import Header from '../../../Components/Header'
 import Loading from '../../../Components/Reusable/Loading'
+import { MdAccountCircle } from 'react-icons/md'
 import AuthContext from '../../../Context/AuthContext'
 import FirebaseAPI from '../../../Context/FirebaseAPI'
 import FirestoreAPI from '../../../Context/FirestoreAPI'
 import { ToastContainer } from 'react-toastify'
-import { useEffect, useContext } from 'react'
+import { useEffect, useContext, useState } from 'react'
 import { useRouter } from 'next/router'
 import Image from 'next/image'
 
@@ -25,17 +26,96 @@ function Project() {
 		<div id='Page'>
 			<ToastContainer />
 			<Header />
-			{isLoading ? (
+			{isLoading || !project ? (
 				<Loading />
 			) : (
 				<>
-					<div id={styles.Admin}>
-						{user && (
-							<main>
+					{user && (
+						<main id={styles.Project}>
+							<div className={styles.Heading}>
 								<h1>{project.Project}</h1>
-							</main>
-						)}
-					</div>
+								<div className={styles.Stats}>
+									<span>
+										<MdAccountCircle />
+										<h3>{`${project.Users} Users`}</h3>
+									</span>
+									<span>
+										<MdAccountCircle />
+										<h3>{`${project.Forms} Forms`}</h3>
+									</span>
+									<span>
+										<MdAccountCircle />
+										<h3>{`${project.Automations} Automations`}</h3>
+									</span>
+									<span>
+										<MdAccountCircle />
+										<h3>{`${project.Reports} Reports`}</h3>
+									</span>
+								</div>
+								<p>{project.Description}</p>
+								<div className={styles.Images}>
+									<div className={styles.MockUp}>
+										<Image
+											src={project?.Company?.Logo}
+											alt={`${project?.Company?.Company} Logo`}
+											width={project?.Company?.id === 'EMN' ? '175px' : '25px'}
+											height='25px'
+											className={styles.Company}
+										/>
+										{project?.Company?.id !== 'EMN' && (
+											<h4>{project?.Company.Company}</h4>
+										)}
+									</div>
+									{project?.MockUpImg ? (
+										<Image
+											className={styles.MockUp}
+											src={project?.MockUpImg}
+											alt={`${project.Project} Image`}
+											width='300px'
+											height='225px'
+										/>
+									) : (
+										<Image
+											className={styles.MockUp}
+											src='/img/MockUp.png'
+											alt={`${project.Project} Image`}
+											width='300px'
+											height='225px'
+										/>
+									)}
+									<div className={styles.Gallery}>
+										{project.Images &&
+											project?.Images.map((image, index) => {
+												return (
+													<Image
+														key={image}
+														src={image}
+														alt={`Image ${index}`}
+														width='100px'
+														height='75px'
+													/>
+												)
+											})}
+									</div>
+								</div>
+								<div className={styles.Technology}>
+									<h3>Technology Used:</h3>
+									{project?.Technology?.map((tech) => {
+										return (
+											<Image
+												key={tech}
+												src={`/Icons/${tech}.png`}
+												alt={tech}
+												width='30px'
+												height='30px'
+											/>
+										)
+									})}
+								</div>
+							</div>
+							<div className={styles.Details}></div>
+						</main>
+					)}
 				</>
 			)}
 		</div>
